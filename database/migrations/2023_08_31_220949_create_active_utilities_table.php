0c821920f919012c61657f7bf157c75356e511b6
lodekib@gmail.com <lodekib@gmail.com>
2023-09-01 01:15:01 +0300

    active utilites model

---
+++ database/migrations/2023_08_31_220949_create_active_utilities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('active_utilities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('manager_id');
+            $table->unsignedBigInteger('tenant_id');
+            $table->string('tenant_name',100);
+            $table->string('property_name');
+            $table->json('active_utilities');
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('active_utilities');
+    }
+};
