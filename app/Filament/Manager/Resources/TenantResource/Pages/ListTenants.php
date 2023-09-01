f2d2dc9690d1d0917a9f6c09ce7345a2ebfa4937
lodekib@gmail.com <lodekib@gmail.com>
2023-09-01 18:12:41 +0300

    import tenants

--- app/Filament/Manager/Resources/TenantResource/Pages/ListTenants.php
+++ app/Filament/Manager/Resources/TenantResource/Pages/ListTenants.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Manager\Resources\TenantResource\Pages;
 
 use App\Filament\Manager\Resources\TenantResource;
+use App\Models\Property;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Konnco\FilamentImport\Actions\ImportAction;
+use Konnco\FilamentImport\Actions\ImportField;
 
 class ListTenants extends ListRecords
 {
@@ -14,6 +17,25 @@ class ListTenants extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            ImportAction::make()->uniqueField('id_number')->fields([
+                ImportField::make('full_names')->required(),
+                ImportField::make('email')->required(),
+                ImportField::make('phone_number')->required(),
+                ImportField::make('id_number')->required(),
+                ImportField::make('property_name')->required()->rules('exists:properties,property_name'),
+                ImportField::make('unit_name')->rules('exists:units,unit_name'),
+                ImportField::make('rent'),
+                ImportField::make('deposit'),
+                ImportField::make('balance'),
+                ImportField::make('arrears'),
+                ImportField::make('surplus'),
+                ImportField::make('entry_date'),
+            ], columns: 4)->icon('heroicon-o-arrow-down-tray')
+            ->handleRecordCreation(function ($data) {
+                $property =  Property::where('property_name', $data['property_name'])->pluck('id');
+                $new_data = array_merge($data, ['property_id' => $property[0]]);
+                return  $this->getModel()::create($new_data);
+            }),
         ];
     }
 }
