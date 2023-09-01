6bb8107b4cf9ea475259d920caa87f32cc9a242e
lodekib@gmail.com <lodekib@gmail.com>
2023-09-01 16:34:57 +0300

    invoices importing

--- app/Filament/Manager/Resources/InvoiceResource/Pages/ListInvoices.php
+++ app/Filament/Manager/Resources/InvoiceResource/Pages/ListInvoices.php
@@ -16,48 +16,51 @@ use Konnco\FilamentImport\Actions\ImportField;
 
 class ListInvoices extends ListRecords
 {
-    protected static string $resource = InvoiceResource::class;
+  protected static string $resource = InvoiceResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            // Actions\CreateAction::make(),
-            ImportAction::make()->label('Import Invoices')->uniqueField('invoice_number')->icon('heroicon-o-arrow-down-tray')->fields([
-                ImportField::make('property_name')->required()->rules('exists:properties,property_name'),
-                ImportField::make('unit_name')->required(),
-                ImportField::make('tenant_name'),
-                ImportField::make('tenant_identity')->required()->rules('exists:tenants,id_number'),
-                ImportField::make('invoice_number')->required(),
-                ImportField::make('invoice_type')->required(),
-                ImportField::make('due_date'),
-                ImportField::make('from'),
-                ImportField::make('to'),
-                ImportField::make('amount_invoiced')->required(),
-                ImportField::make('balance')->required(),
-                ImportField::make('invoice_description')
-              ], columns: 4)->handleRecordCreation(function ($data) {
-                $tenant = Tenant::where('id_number', $data['tenant_identity'])->get();
-                $new_data = array_merge($data, [
-                  'tenant_id' => $tenant->first()->id,
-                ]);
-                return $this->getModel()::create($new_data);
-              })->mutateAfterCreate(function (Model $model) {
-                //TODO::SOME OPTIMIZATIONS NEEDED 
-                $tenant = Tenant::find($model->tenant_id);
-                $total_debit = Statement::where('tenant_name', $model->tenant_name)->sum('debit');
-                $total_credit = Statement::where('tenant_name', $model->tenant_name)->sum('credit');
-                $statement_data = [
-                  'tenant_id' => $model->tenant_id,
-                  'tenant_name' => $model->tenant_name,
-                  'description' => $model->invoice_type . " invoice",
-                  'reference' => $model->invoice_number,
-                  'debit' => $model->balance,
-                  'balance' => $total_debit - ($total_credit - $model->balance),
-                  'cummulative_balance' => $total_debit - ($total_credit - $model->balance)
-                ];
-                $statement = Statement::create($statement_data);
-                InvoiceReceiptAutoAllocation::handleNewInvoice($model);
-              }),
+  protected function getHeaderActions(): array
+  {
+    return [
+      // Actions\CreateAction::make(),
+      ImportAction::make()->label('Import Invoices')->uniqueField('invoice_number')->icon('heroicon-o-arrow-down-tray')->fields([
+        ImportField::make('property_name')->required()->rules('exists:properties,property_name'),
+        ImportField::make('unit_name')->required()->rules('exists:units,unit_name'),
+        ImportField::make('tenant_name')->required()->rules('exists:tenants,full_names'),
+        ImportField::make('national_id')->required()->rules('exists:tenants,id_number'),
+        ImportField::make('invoice_number')->required(),
+        ImportField::make('invoice_type')->required(),
+        ImportField::make('due_date'),
+        ImportField::make('from'),
+        ImportField::make('to'),
+        ImportField::make('amount_invoiced')->required(),
+        ImportField::make('balance')->required(),
+        ImportField::make('invoice_description')
+      ], columns: 4)->handleRecordCreation(function ($data) {
+        $tenant = Tenant::where('id_number', $data['national_id'])->pluck('id');
+        $new_data = array_merge($data, [
+          'tenant_id' => $tenant[0],
+          'invoice_status' => ($data['amount_invoiced'] == $data['balance']) ? "pending" : (($data['balance'] == 0) ? "fully_paid" : "partially_paid")
+        ]);
+        return $this->getModel()::create($new_data);
+      })->mutateAfterCreate(function (Model $model) {
+
+        $debit_credit = Statement::selectRaw('tenant_name, SUM(debit) as total_debit, SUM(credit) as total_credit')
+          ->where('tenant_name', $model->tenant_name)
+          ->groupBy('tenant_name')
+          ->first();
+        $statement_data = [
+          'tenant_id' => $model->tenant_id,
+          'tenant_name' => $model->tenant_name,
+          'description' => $model->invoice_type . " invoice",
+          'reference' => $model->invoice_number,
+          'debit' => $model->balance,
+          'credit' => 0,
+          'balance' => $debit_credit != null ? $debit_credit->total_debit - ($debit_credit->total_credit - $model->balance) : $model->balance,
+          'cummulative_balance' => $debit_credit != null ? $debit_credit->total_debit - ($debit_credit->total_credit - $model->balance) : $model->balance
         ];
-    }
+        Statement::create($statement_data);
+        InvoiceReceiptAutoAllocation::handleNewInvoice($model);
+      }),
+    ];
+  }
 }
