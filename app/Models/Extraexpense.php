cb78011eed628f43b0b89f4d1fd87a1d6d7b082b
lodekib@gmail.com <lodekib@gmail.com>
2023-09-14 16:59:49 +0300

    fixed relationship of expenses with extra expenses

--- app/Models/Extraexpense.php
+++ app/Models/Extraexpense.php
@@ -5,11 +5,16 @@ namespace App\Models;
 use App\Traits\HasManager;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Extraexpense extends Model
 {
     use HasFactory, HasManager;
 
     protected $guarded = [];
-    
+
+    public function expense(): BelongsTo
+    {
+        return $this->belongsTo(Expense::class);
+    }
 }
