dde85a26d5838bbc243f6d62212316a984fcd1d4
lodekib@gmail.com <lodekib@gmail.com>
2023-09-28 12:39:37 +0300

    mpesa fixed

--- app/Filament/Manager/Pages/ArchivedPayments.php
+++ app/Filament/Manager/Pages/ArchivedPayments.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Manager\Pages;
 
 use App\Models\Payment;
+use App\Models\Tenant;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Pages\Page;
@@ -11,7 +12,8 @@ use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-
+use Konnco\FilamentImport\Actions\ImportAction;
+use Konnco\FilamentImport\Actions\ImportField;
 
 class ArchivedPayments extends Page implements HasForms, HasTable
 {
@@ -22,8 +24,8 @@ class ArchivedPayments extends Page implements HasForms, HasTable
 
     public function table(Table $table): Table
     {
-        return $table->query(Payment::query()->where('status','like','stale/%'))->modifyQueryUsing(fn(Builder $query) => $query->withoutGlobalScope('nonestale'))->columns([
-            TextColumn::make('status')->badge()->formatStateUsing(fn() => 'stale')->color('danger')->searchable(),
+        return $table->query(Payment::query()->where('status', 'like', 'stale/%'))->modifyQueryUsing(fn (Builder $query) => $query->withoutGlobalScope('nonestale'))->columns([
+            TextColumn::make('status')->badge()->formatStateUsing(fn () => 'stale')->color('danger')->searchable(),
             TextColumn::make('paid_date')->size('sm')->date()->searchable(),
             TextColumn::make('tenant_name')->size('sm')->searchable()->sortable(),
             TextColumn::make('receipt_number')->size('sm')->sortable()->searchable(),
