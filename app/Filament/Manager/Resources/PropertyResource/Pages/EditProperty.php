31cbbc6c974c4f627ccad8e649e99a5278b79298
lodekib@gmail.com <lodekib@gmail.com>
2023-10-11 11:08:01 +0300

    on property_edit cascade

--- app/Filament/Manager/Resources/PropertyResource/Pages/EditProperty.php
+++ app/Filament/Manager/Resources/PropertyResource/Pages/EditProperty.php
@@ -5,6 +5,7 @@ namespace App\Filament\Manager\Resources\PropertyResource\Pages;
 use App\Filament\Manager\Resources\PropertyResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditProperty extends EditRecord
 {
@@ -20,5 +21,19 @@ class EditProperty extends EditRecord
     public function getRelationManagers(): array
     {
         return [];
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+
+        $record->units()->update(['property_name' => $data['property_name']]);
+        $record->tenants()->update(['property_name' => $data['property_name']]);
+        $record->payments()->update(['property_name' => $data['property_name']]);
+        $record->invoices()->update(['property_name' => $data['property_name']]);
+        $record->utilities()->update(['property_name' => $data['property_name']]);
+        $record->expenses()->update(['property_name' => $data['property_name']]);
+
+        $record->update($data);
+        return $record;
     }
 }
--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\HasManager;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Expense extends Model
@@ -17,5 +18,10 @@ class Expense extends Model
     public function extraexpenses(): HasMany
     {
         return $this->hasMany(Extraexpense::class);
+    }
+
+    public function property(): BelongsTo
+    {
+        return $this->belongsTo(Property::class, 'property_name', 'property_name');
     }
 }
