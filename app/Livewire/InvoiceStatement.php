2880162384397bf32183a52cf1ed8fff3eccdd75
lodekib@gmail.com <lodekib@gmail.com>
2023-10-16 19:35:16 +0300

    invoices on the invoice statement

--- app/Livewire/InvoiceStatement.php
+++ app/Livewire/InvoiceStatement.php
@@ -3,6 +3,9 @@
 namespace App\Livewire;
 
 use App\Models\Invoice;
+use App\Models\Property;
+use App\Models\Utility;
+use App\Models\Waterbill;
 use Carbon\Carbon;
 use Livewire\Component;
 
@@ -17,8 +20,11 @@ class InvoiceStatement extends Component
         $pending_invoices = Invoice::where('tenant_name', $this->record->full_names)
             ->where('invoice_status', '!=', 'fully paid')
             ->whereBetween('created_at', [Carbon::parse($currentMonth . '-01 00:00:00'), Carbon::parse($currentMonth . '-31 23:59:59')])
-            ->get(['invoice_type', 'balance', 'created_at']);
-        return $pending_invoices;
+            ->get(['invoice_type', 'balance']);
+        $water_readings = Waterbill::where('tenant_id', $this->record->id)->latest()->get(['previous_reading', 'current_reading']);
+        $property = $this->record->property;
+        $rate = Utility::where('property_name', $property->property_name)->where('utility_name', 'Water')->get('amount')->first();
+        return [$pending_invoices, $water_readings->first(),$rate];
     }
 
 
--- resources/views/livewire/invoice-statement.blade.php
+++ resources/views/livewire/invoice-statement.blade.php
@@ -3,7 +3,7 @@
             <th colspan="7" class="bg-gray-60 text-center py-4"> {{ $record->full_names }} </th>
         </tr>
         <tr>
-            <th colspan="7" class="bg-gray-60 text-center py-4"> {{ $this->invoices() }} </th>
+            <th colspan="7" class="bg-gray-60 text-center py-4"> {{ $this->invoices()[2]['amount']}} </th>
         </tr>
         <tr>
             <td class="px-4 py-2" colspan="6">Balance as of 30th September 2023</td>
@@ -13,7 +13,7 @@
             <td colspan="4" class="font-bold text-center">INVOICE</td>
             <td colspan="3" class="font-bold text-center">AMOUNT</td>
         </tr>
-        @foreach ($this->invoices() as $invoice )
+        @foreach ($this->invoices()[0] as $invoice )
         @if($invoice->invoice_type !=='Water')
         <tr>
             <td class="px-4 py-2" colspan="6">{{ $invoice->invoice_type }}</td>
@@ -21,17 +21,22 @@
         </tr>
         @endif
         @endforeach
+        @foreach ($this->invoices()[0] as $invoice )
+        @if($invoice->invoice_type === 'Water')
         <tr>
-            <td class="px-4 py-2" colspan="6">Water Bill</td>
-            <td class="px-4 py-2" colspan="1">KES {{number_format(1200)}}</td>
+            <td class="px-4 py-2" colspan="6">{{ $invoice->invoice_type }}</td>
+            <td class="px-4 py-2" colspan="1">KES {{number_format($invoice->balance)}}</td>
         </tr>
+        @endif
+        @endforeach
+        @if($this->invoices()[1] !== null)
         <tr>
             <td></td>
             <td></td>
             <td class="px-4 py-2">Current Reading (m3)</td>
-            <td class="px-4 py-2">{{number_format(23)}}</td>
+            <td class="px-4 py-2">{{number_format($this->invoices()[1]->current_reading)}}</td>
             <td class="px-4 py-2">Consumption (m3)</td>
-            <td class="px-4 py-2">{{number_format(10)}}</td>
+            <td class="px-4 py-2">{{number_format($this->invoices()[1]->current_reading - $this->invoices()[1]->previous_reading)}}</td>
             <td></td>
             <td></td>
         </tr>
@@ -39,12 +44,13 @@
             <td></td>
             <td></td>
             <td class="px-4 py-2">Previous Reading (m3)</td>
-            <td class="px-4 py-2">{{number_format(13)}}</td>
+            <td class="px-4 py-2">{{ number_format($this->invoices()[1]->previous_reading) }}</td>
             <td class="px-4 py-2">Rate (KES)</td>
-            <td class="px-4 py-2">KES {{number_format(120)}}</td>
+            <td class="px-4 py-2">KES {{number_format($this->invoices()[2]['amount'])}}</td>
             <td></td>
             <td></td>
         </tr>
+        @endif
         <tr>
             <td colspan="7" class="px-4 py-2 text-center">Other Utilities</td>
         </tr>
