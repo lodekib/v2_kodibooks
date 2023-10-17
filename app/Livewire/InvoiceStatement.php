34e345141ccab11416b5f8ccb2bad6c72dc833eb
lodekib@gmail.com <lodekib@gmail.com>
2023-10-17 10:23:30 +0300

    finished the invoice statement

--- app/Livewire/InvoiceStatement.php
+++ app/Livewire/InvoiceStatement.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Invoice;
 use App\Models\Property;
+use App\Models\Statement;
 use App\Models\Utility;
 use App\Models\Waterbill;
 use Carbon\Carbon;
@@ -16,15 +17,24 @@ class InvoiceStatement extends Component
     public function invoices()
     {
         $currentMonth = Carbon::now()->format('Y-m');
+        $startOfMonth = Carbon::now()->startOfMonth();
 
         $pending_invoices = Invoice::where('tenant_name', $this->record->full_names)
             ->where('invoice_status', '!=', 'fully paid')
             ->whereBetween('created_at', [Carbon::parse($currentMonth . '-01 00:00:00'), Carbon::parse($currentMonth . '-31 23:59:59')])
             ->get(['invoice_type', 'balance']);
+        $total_balances = Invoice::where('tenant_name', $this->record->full_names)
+            ->where('invoice_status', '!=', 'fully paid')
+            ->whereBetween('created_at', [Carbon::parse($currentMonth . '-01 00:00:00'), Carbon::parse($currentMonth . '-31 23:59:59')])
+            ->sum('balance');
         $water_readings = Waterbill::where('tenant_id', $this->record->id)->latest()->get(['previous_reading', 'current_reading']);
         $property = $this->record->property;
         $rate = Utility::where('property_name', $property->property_name)->where('utility_name', 'Water')->get('amount')->first();
-        return [$pending_invoices, $water_readings->first(),$rate];
+        $balance = Statement::where('tenant_name', $this->record->full_names)
+            ->selectRaw('SUM(debit) - SUM(credit) as balance')->where('created_at', '<', $startOfMonth)->first()->balance;
+        $consumption_total = ($water_readings->first()->current_reading - $water_readings->first()->previous_reading) * $rate->amount;
+        $total_overdue = $balance + $consumption_total + $total_balances;
+        return [$pending_invoices, $water_readings->first(), $rate, $balance, $total_overdue];
     }
 
 
--- resources/views/livewire/invoice-statement.blade.php
+++ resources/views/livewire/invoice-statement.blade.php
@@ -3,18 +3,15 @@
             <th colspan="7" class="bg-gray-60 text-center py-4"> {{ $record->full_names }} </th>
         </tr>
         <tr>
-            <th colspan="7" class="bg-gray-60 text-center py-4"> {{ $this->invoices()[2]['amount']}} </th>
-        </tr>
-        <tr>
-            <td class="px-4 py-2" colspan="6">Balance as of 30th September 2023</td>
-            <td class="px-4 py-2" colspan="1">KES {{number_format(34000)}}</td>
+            <td class="px-4 py-2" colspan="6">Balance ,by {{\Carbon\Carbon::now()->subMonthNoOverflow()->endOfMonth()->format('F d,Y') }}</td>
+            <td class="px-4 py-2" colspan="1">KES {{number_format($this->invoices()[3])}}</td>
         </tr>
         <tr>
             <td colspan="4" class="font-bold text-center">INVOICE</td>
             <td colspan="3" class="font-bold text-center">AMOUNT</td>
         </tr>
         @foreach ($this->invoices()[0] as $invoice )
-        @if($invoice->invoice_type !=='Water')
+        @if($invoice->invoice_type === 'Rent' )
         <tr>
             <td class="px-4 py-2" colspan="6">{{ $invoice->invoice_type }}</td>
             <td class="px-4 py-2" colspan="1">KES {{number_format($invoice->balance)}}</td>
@@ -27,8 +24,6 @@
             <td class="px-4 py-2" colspan="6">{{ $invoice->invoice_type }}</td>
             <td class="px-4 py-2" colspan="1">KES {{number_format($invoice->balance)}}</td>
         </tr>
-        @endif
-        @endforeach
         @if($this->invoices()[1] !== null)
         <tr>
             <td></td>
@@ -51,19 +46,21 @@
             <td></td>
         </tr>
         @endif
+        @endif
+        @endforeach
         <tr>
             <td colspan="7" class="px-4 py-2 text-center">Other Utilities</td>
         </tr>
+        @foreach ($this->invoices()[0] as $invoice )
+        @if($invoice->invoice_type !=='Water' || $invoice->invoice_type !== 'Rent')
         <tr>
-            <td class="px-4 py-2" colspan="6">Garbage</td>
-            <td class="px-4 py-2" colspan="1">KES {{number_format(870)}}</td>
+            <td class="px-4 py-2" colspan="6">{{ $invoice->invoice_type }}</td>
+            <td class="px-4 py-2" colspan="1">KES {{number_format($invoice->balance)}}</td>
         </tr>
-        <tr>
-            <td class="px-4 py-2" colspan="6">Service</td>
-            <td class="px-4 py-2" colspan="1">KES {{number_format(1000)}}</td>
-        </tr>
+        @endif
+        @endforeach
         <tr>
             <td class="px-4 py-2 font-bold" colspan="6">AMOUNT DUE :</td>
-            <td class="px-4 py-2 font-bold" colspan="1">KES {{number_format(60070)}}
+            <td class="px-4 py-2 font-bold" colspan="1">KES {{number_format($this->invoices()[4])}}
         </tr>
     </table>
