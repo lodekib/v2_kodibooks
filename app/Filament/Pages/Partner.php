8d5cff857f5c8a2bd1b26dd33fb93f8da17d08ac
Lodeki Brian <lodekib@gmail.com>
2023-11-28 20:08:20 +0300

    partner approval and emailing

--- app/Filament/Pages/Partner.php
+++ app/Filament/Pages/Partner.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Pages;
 
+use App\Mail\PartnerApproved;
 use App\Models\User;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Notifications\Notification;
 use Filament\Pages\Page;
 use Filament\Tables\Actions\Action;
 use Filament\Tables\Actions\ActionGroup;
@@ -13,6 +15,8 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
+use Illuminate\Support\Facades\Mail;
+use PhpOffice\PhpSpreadsheet\Calculation\Logical\Boolean;
 
 class Partner extends Page implements HasForms, HasTable
 {
@@ -28,18 +32,33 @@ class Partner extends Page implements HasForms, HasTable
                 TextColumn::make('name')->label('Partner name')->size('sm')->searchable(),
                 TextColumn::make('email')->size('sm')->searchable()->sortable(),
                 TextColumn::make('is_verified')->label('Status')->badge()->formatStateUsing(fn ($state) => $state == 0 ? 'pending approval' : 'Approved')
-                    ->icon(fn (string $state): string => match ($state) {
-                        '0' => 'heroicon-o-clock',
-                        '1' => 'heroicon-o-check',
-                    })->color(fn (string $state): string => match ($state) {
-                        '0' => 'danger',
-                        '1' => 'info',
+                    ->icon(fn (string $state): string => match ((bool)$state) {
+                        false => 'heroicon-o-clock',
+                        true => 'heroicon-o-check',
+                    })->color(fn (string $state): string => match ((bool)$state) {
+                        false => 'warning',
+                        true => 'info',
                     }),
                 // ImageColumn::make('partners.kyc')->size('sm')->state(function($record,$state){
                 // })->stacked(),
                 // TextColumn::make('partners.commission')->label('Commission')->size('sm')->formatStateUsing(fn ($state) => $state == null ? '-' : $state)
             ])->actions([
-                Action::make('Approve Partner')->color('gray')->outlined()->icon('heroicon-o-shield-check')->button()->action(function () {})
+                Action::make('Approve Partner')->color('gray')->outlined()->icon('heroicon-o-shield-check')->button()->action(function ($record) {
+                    $status = $record->is_verified;
+                    if ((bool)$status == false) {
+                        $record->is_verified = true;
+                        $record->save();
+                        $mail = Mail::to($record)->send(new PartnerApproved($record));
+                        if ($mail) {
+                            Notification::make()->color('success')->body('Partner verified successfully !')->send();
+                        }
+                    } else {
+                        $record->is_verified = false;
+                        $record->save();
+                        //send email
+                        Notification::make()->color('danger')->body('Partner unapproved !')->send();
+                    }
+                })
             ]);
     }
 }
