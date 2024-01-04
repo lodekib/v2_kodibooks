9fcadc3fc87d2aee677fc045e9527ccae549aa18
Lodeki Brian <lodekib@gmail.com>
2024-01-04 10:41:15 +0300

    landing page

--- resources/views/mail/client-invoiced.blade.php
+++ resources/views/mail/client-invoiced.blade.php
@@ -1,7 +1,11 @@
-<x-mail::message>
-# Subscription Fee
-
-Please make the payment for the invoice attached for you to use the service efficiently.
-Thank you,<br>
-{{ config('app.name') }}
-</x-mail::message>
+<div style="text-align: center; padding: 20px; border: 2px solid #00FF00; border-radius: 10px; max-width: 600px; margin: 0 auto;">
+    <img src="assets/logo.png" alt="Your Logo" style="max-width: 100px; margin-bottom: 20px;">
+    <h1 style="color: #00FF00; font-size: 24px;">Subscription Fee</h1>
+    <p style="color: #333; font-size: 16px; line-height: 1.5;">
+        Please make the payment for the invoice attached for you to use the service efficiently.
+    </p>
+    <p style="color: #333; font-size: 16px; line-height: 1.5;">
+        Thank you,<br>
+        {{ config('app.name') }}
+    </p>
+</div>
