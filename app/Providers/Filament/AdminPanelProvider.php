e7fefc379418e6819ed4070b894753f69e7dddb2
Lodeki Brian <lodekib@gmail.com>
2024-02-09 21:03:41 +0300

    activity logs

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,8 @@ class AdminPanelProvider extends PanelProvider
             ->login()->passwordReset()->emailVerification()->registration()
             ->colors([
                 'primary' => Color::hex('#4ade80'),
+            ])->resources([
+                config('filament-logger.activity_resource')
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
