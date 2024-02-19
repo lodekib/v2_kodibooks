37dc4097eabf7e3fba350599ac0bc00d0be28ded
Lodeki Brian <lodekib@gmail.com>
2024-02-19 14:54:50 +0300

    the client payments

--- app/Filament/Pages/ClientPayments.php
+++ app/Filament/Pages/ClientPayments.php
@@ -18,9 +18,14 @@ class ClientPayments extends Page implements HasTable
 
     public function table(Table $table): Table
     {
-        return $table->query(MpesaC2B::query())
+        return $table->query(MpesaC2B::query()->latest())
             ->columns([
-                TextColumn::make('created_at')->size('sm')->searchable()->sortable()
+                TextColumn::make('created_at')->size('sm')->searchable()->sortable(),
+                TextColumn::make('FirstName')->size('sm')->searchable()->sortable()->wrap(),
+                TextColumn::make('Transaction_ID')->size('sm')->searchable()->sortable()->copyable(),
+                TextColumn::make('Amount')->size('sm'),
+                TextColumn::make('Account_Number')->size('sm')->copyable()->searchable()->sortable(),
+                TextColumn::make('Phonenumber')->size('sm')->copyable()->searchable()->sortable(),
             ]);
     }
 }
